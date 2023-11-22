82122b87f62e90bb9c9ec40a5ea2f80a40e7d4bc
DarkForest571 <nikitalebed571@gmail.com>
2023-11-22 22:04:45 +1000

    Upgrade Vector2

--- Match3/Utils/Vector2.cs
+++ Match3/Utils/Vector2.cs
@@ -1,82 +1,75 @@
-﻿namespace Match3.Utils
+﻿using System.Numerics;
+
+namespace Match3.Utils
 {
-    public struct Vector2
+    public struct Vector2<T> where T : INumber<T>
     {
-        public int X, Y;
+        public T X, Y;
 
-        public Vector2(int x = 0, int y = 0)
+        public Vector2(T? x = default, T? y = default)
         {
-            (X, Y) = (x, y);
+            if (x is null || y is null)
+                throw new ArgumentNullException();
+            X = x;
+            Y = y;
         }
 
-        public Vector2(Point point)
+        public readonly bool IsNeighbor(Vector2<T> vector)
         {
-            (X, Y) = (point.X, point.Y);
-        }
-
-        public readonly bool IsNeighbor(Vector2 vector)
-        {
-            Vector2 delta = this - vector;
+            Vector2<T> delta = this - vector;
             if (delta == Up || delta == Down || delta == Left || delta == Right)
                 return true;
             return false;
         }
 
-        private static Vector2 _zero = new (0, 0);
-        private static Vector2 _one = new (1, 1);
+        private static Vector2<T> _zero = new(T.Zero, T.Zero);
+        private static Vector2<T> _one = new(T.One, T.One);
 
-        private static Vector2 _up = new (0, -1);
-        private static Vector2 _down = new (0, 1);
-        private static Vector2 _left = new (-1, 0);
-        private static Vector2 _right = new (1, 0);
+        private static Vector2<T> _up = new(T.Zero, -T.One);
+        private static Vector2<T> _down = new(T.Zero, T.One);
+        private static Vector2<T> _left = new(-T.One, T.Zero);
+        private static Vector2<T> _right = new(T.One, T.Zero);
 
-        public static Vector2 Zero => _zero;
-        public static Vector2 One => _one;
+        public static Vector2<T> Zero => _zero;
+        public static Vector2<T> One => _one;
 
-        public static Vector2 Up => _up;
-        public static Vector2 Down => _down;
-        public static Vector2 Left => _left;
-        public static Vector2 Right => _right;
+        public static Vector2<T> Up => _up;
+        public static Vector2<T> Down => _down;
+        public static Vector2<T> Left => _left;
+        public static Vector2<T> Right => _right;
 
-        public static Vector2[] AllDirections => new Vector2[] { Up, Down, Left, Right };
+        public static Vector2<T>[] AllDirections => new Vector2<T>[] { Up, Down, Left, Right };
 
-        public static Vector2 operator -(Vector2 vector) => new(-vector.X, -vector.Y);
+        public static Vector2<T> operator -(Vector2<T> vector) => new(-vector.X, -vector.Y);
 
-        public static bool operator <(Vector2 left, Vector2 right) => left.X < right.X && left.Y < right.Y;
+        public static bool operator <(Vector2<T> left, Vector2<T> right) => left.X < right.X && left.Y < right.Y;
 
-        public static bool operator >(Vector2 left, Vector2 right) => left.X > right.X && left.Y > right.Y;
+        public static bool operator >(Vector2<T> left, Vector2<T> right) => left.X > right.X && left.Y > right.Y;
 
-        public static bool operator <=(Vector2 left, Vector2 right) => left.X <= right.X && left.Y <= right.Y;
+        public static bool operator <=(Vector2<T> left, Vector2<T> right) => left.X <= right.X && left.Y <= right.Y;
 
-        public static bool operator >=(Vector2 left, Vector2 right) => left.X >= right.X && left.Y >= right.Y;
+        public static bool operator >=(Vector2<T> left, Vector2<T> right) => left.X >= right.X && left.Y >= right.Y;
 
-        public static bool operator ==(Vector2 left, Vector2 right) => left.X == right.X && left.Y == right.Y;
+        public static bool operator ==(Vector2<T> left, Vector2<T> right) => left.X == right.X && left.Y == right.Y;
 
-        public static bool operator !=(Vector2 left, Vector2 right) => !(left == right);
-
-
-        public static Vector2 operator -(Vector2 left, Vector2 right) => new(left.X - right.X, left.Y - right.Y);
-
-        public static Vector2 operator +(Vector2 left, Vector2 right) => new(left.X + right.X, left.Y + right.Y);
-
-        public static Vector2 operator +(Vector2 left, Direction right) =>
-            left + FromDirection(right);
+        public static bool operator !=(Vector2<T> left, Vector2<T> right) => !(left == right);
 
 
-        public static Vector2 operator *(Vector2 left, int right) => new(left.X * right, left.Y * right);
+        public static Vector2<T> operator -(Vector2<T> left, Vector2<T> right) => new(left.X - right.X, left.Y - right.Y);
 
-        public static Vector2 operator *(int left, Vector2 right) => right * left;
+        public static Vector2<T> operator +(Vector2<T> left, Vector2<T> right) => new(left.X + right.X, left.Y + right.Y);
 
-        public static Vector2 operator /(Vector2 left, int right) => new(left.X / right, left.Y / right);
+        public static Vector2<T> operator +(Vector2<T> left, Direction right) => left + FromDirection(right);
 
 
-        public static Vector2 operator *(Vector2 left, float right) => new((int)(left.X * right), (int)(left.Y * right));
+        public static Vector2<T> operator *(Vector2<T> left, T right) => new(left.X * right, left.Y * right);
 
-        public static Vector2 operator *(float left, Vector2 right) => right * left;
+        public static Vector2<T> operator *(T left, Vector2<T> right) => right * left;
 
-        public static Vector2 operator /(Vector2 left, float right) => new((int)(left.X / right), (int)(left.Y / right));
+        public static Vector2<T> operator /(Vector2<T> left, T right) => new(left.X / right, left.Y / right);
 
-        public static Vector2 FromDirection(Direction direction) =>
+
+        public static Vector2<T> FromDirection(Direction direction) =>
             direction switch
             {
                 Direction.Up => Up,
@@ -87,7 +80,11 @@
                 _ => throw new NotImplementedException()
             };
 
-        public void Deconstruct(out int X, out int Y)
+        public readonly void Deconstruct(out T X, out T Y) => (X, Y) = (this.X, this.Y);
+
+        public override readonly bool Equals(object? obj) => obj is Vector2<T> && this == (Vector2<T>)obj;
+
+        public override int GetHashCode()
         {
             throw new NotImplementedException();
         }
